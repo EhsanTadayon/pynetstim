1e472c01dbbf3ee2ad129ca09f2d5ebbaa951a60
Ehsan Tadayon <stadayon@ALE4B1E5.its.caregroup.org>
2018-09-30 20:01:25 -0400

    adds docs

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pynetstim',
+      version='0.1',
+      description='perturbation of human resting state networks',
+      author='Ehsan Tadayon,M.D.',
+      author_email='sunny.tadayon@gmail.com',
+      license='MIT',
+      packages=['pynetstim'],
+      zip_safe=False)
+
+
