fcc03fd1d8940e798a73ce193395244d66918242
Ehsan Tadayon <stadayon@ALE4B1E5.its.caregroup.org>
2018-10-16 17:23:35 -0400

    first working script for simnibs simulation

---
+++ pynetstim/tms_simulation.py
@@ -0,0 +1,99 @@
+#### registration module 
+from nipype.interfaces.fsl import FLIRT
+from nipype import Workflow, Node
+import os
+from targets import Targets
+from coordinates import Coords
+from simnibs import sim_struct, run_simulation
+from surface import Surf
+
+class Simnibs(object):
+    
+    SIMNIBSDIR='/Users/stadayon/simnibs_2.1.1'
+    def __init__(self, subject, simnibs_dir, out_dir):
+        self.subject = subject
+        self.simnibs_dir = simnibs_dir
+        self.out_dir = out_dir
+        
+        
+        
+    def register_T1_to_simnibs(self):
+        
+        flirt = Node(FLIRT(),name='flirt')
+        flirt.inputs.in_file = os.path.join(self.simnibs_dir,'m2m_'+self.subject,'T1fs.nii.gz')
+        flirt.inputs.reference = os.path.join(self.simnibs_dir, 'm2m_'+self.subject,'T1fs_conform.nii.gz')
+        flirt.inputs.out_file = 'T1_in_Simnibs.nii.gz'
+        flirt.inputs.out_matrix_file = 'T12Simnibs.mat'
+        flirt.inputs.searchr_x = [-180,180]
+        flirt.inputs.searchr_y = [-180,180]
+        flirt.inputs.searchr_z = [-180,180]
+    
+        wf = Workflow(name='T1_to_simnibs_registration',base_dir=self.out_dir)
+        wf.add_nodes([flirt])
+        wf.run()
+        
+    def add_targets(self, ras_coords, directions,targets_csv_fname,t12simnibs=False, project_to_skin=True, distance=4, names=None, colors=None):
+        
+        if not os.path.exists(os.path.join(self.out_dir,'T1_to_simnibs_registration')):
+            self.register_T1_to_simnibs()
+        
+        if t12simnibs:
+            coordsObj = Coords(ras_coords,os.path.join(self.simnibs_dir,'m2m_'+self.subject,'T1fs.nii.gz'))
+            t12simnibs_reg = os.path.join(self.out_dir,'T1_to_simnibs_registration','flirt','T12Simnibs.mat')
+            ras_coords = coordsObj.img2imgcoord(os.path.join(self.simnibs_dir, 'm2m_'+self.subject,'T1fs_conform.nii.gz'),t12simnibs_reg,type='xfm')
+            
+        if project_to_skin:
+            skin_file = os.path.join(self.simnibs_dir,'fs_'+self.subject,'bem','lh.surf_outer_skin_surface')
+            skin_surf = Surf(skin_file)
+            mapped_vertices, ras_coords = skin_surf.project_coords(ras_coords)
+            
+        self.targets = Targets(ras_coords,'fs_'+self.subject, self.simnibs_dir, names=names, colors=colors, directions=directions)
+        
+         
+        self.targets_csv_fname = targets_csv_fname   
+        self._targets_to_csv(targets_csv_fname)
+            
+            
+    def _targets_to_csv(self, fname, dist=4):
+        
+        if not os.path.exists(os.path.join(self.out_dir,'simnibs_targets')):
+            os.makedirs(os.path.join(self.out_dir,'simnibs_targets'))
+            
+        f = open(os.path.join(self.out_dir,'simnibs_targets',fname),'w')
+            
+        for target in self.targets:
+            if target.name is None:
+                target.name='tms_target'
+                
+            row = ['CoilPos']+ target.ras_coord.tolist() + target.direction.tolist()[6:] + target.direction.tolist()[3:6] + [dist] + [target.name]
+            row = [str(x) for x in row]
+            f.write(','.join(row)+'\n')
+        
+        
+    
+    def run_simnibs(self, sim_output_name,fnamecoil='MagVenture_MC_B70.ccd',overwrite=False):
+        
+        s = sim_struct.SESSION()
+        s.fnamehead = os.path.join(self.simnibs_dir,self.subject+'.msh')
+        try:
+            if overwrite:
+               os.rmdir(os.path.join(self.out_dir,'simnibs_simulations',sim_output_name)) 
+            os.makedirs(os.path.join(self.out_dir,'simnibs_simulations',sim_output_name))
+        except:
+            raise 
+        s.pathfem = os.path.join(self.out_dir,'simnibs_simulations',sim_output_name)
+        s.map_to_vol = True
+        s.map_to_surf = True
+        tms = s.add_tmslist()
+        tms.fnamecoil = os.path.join(Simnibs.SIMNIBSDIR,'ccd-files',fnamecoil)
+        tms.add_positions_from_csv(os.path.join(self.out_dir,'simnibs_targets',self.targets_csv_fname))
+        run_simulation(s)
+    
+            
+    
+        
+    
+            
+        
+
+    
