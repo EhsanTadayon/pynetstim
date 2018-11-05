9e7f43068313342ed67c7564a5eb728c6cd665f8
Ehsan Tadayon <stadayon@ALE4B1E5.its.caregroup.org>
2018-11-05 14:17:39 -0500

    fixing bugs in project module

--- pynetstim/project.py
+++ pynetstim/project.py
@@ -12,6 +12,8 @@ from nipype.interfaces.fsl import FLIRT,FNIRT
 from plotting import plotting_points_fast
 import numpy as np
 from scipy.spatial.distance import cdist
+from utils import make_head_model
+import shutil
 
 
 ## TO DO:
@@ -21,73 +23,116 @@ from scipy.spatial.distance import cdist
 class StimProject(object):
     
     """ main stimulation project class"""
-    def __init__(self, subject, project_dir, anatomical = None, freesurfer_dir = None, simnibs_dir=None, brainsight_file = None, to_mni = False,
-     mni_template='MNI152_T1_2mm.nii.gz', mni_directory = os.path.join(os.environ['FSLDIR'],'data/standard')):
+    def __init__(self, subject, project_dir, freesurfer_dir=None, simnibs_dir=None, brainsight_file = None, to_mni = False,
+     mni_template='MNI152_T1_2mm.nii.gz', mni_dir = os.path.join(os.environ['FSLDIR'],'data/standard'), overwrite=None):
         
         self.subject = subject
 
-        if not os.path.isabs(project_dir):
-            raise ValueError('please provide absolute path for project_dir')
-        if not os.path.isabs(freesurfer_dir):
-            raise ValueError('please provide absolute path for freesurfer_dir')
-            
-        self.anatomical = anatomical
-        self.project_dir = project_dir
-        self.subject_dir = os.path.join(self.project_dir,subject)
-        self.freesurfer_dir = freesurfer_dir
         self.simnibs_dir = simnibs_dir
-        
         if self.simnibs_dir is not None:
             self.freesurfer_dir = self.simnibs_dir
+        else:
+            self.freesurfer_dir = freesurfer_dir
             
-        self.brainsight_file = brainsight_file
-        self.targets = []
+        self.project_dir = project_dir  
+        self.brainsight_file = brainsight_file    
+        self.subject_dir = os.path.join(self.project_dir,subject)
+        self._to_mni=to_mni
+        self._mni_template = mni_template
+        self._mni_dir = mni_dir 
+        self.overwrite=overwrite
+        self.directories = {}
     
         self._start_project()
         
-        if to_mni:
-            self._to_mni(mni_template)    
-            
-        self._read_brainsight_file()
-        
     def _start_project(self):
         
-        # TO DO: add more folders
-        if not os.path.exists(self.subject_dir):
-            os.makedirs('{subject_dir}/brainsight'.format(subject_dir=self.subject_dir))
-            os.makedirs('{subject_dir}/figures'.format(subject_dir = self.subject_dir))
-            os.makedirs('{subject_dir}/mni'.format(subject_dir = self.subject_dir))
-            os.makedirs('{subject_dir}/')
-        
-        self.brainsight_dir='{subject_dir}/brainsight'.format(subject_dir=self.subject_dir)
-        self.figures_dir = '{subject_dir}/figures'.format(subject_dir=self.subject_dir)  
-        self.mni_nonlinear = '{subject_dir}/mni'.format(subject_dir=self.subject_dir)
-        
+        self._start_log()
+            
+        if self.brainsight_file is not None:
+            self._read_brainsight_file()
+              
+        if self._to_mni:
+            self._to_mni(self.mni_template, self.mni_dir)
+                
         ### create head models
-        self._make_head_model()
+        self._make_head_model()    
         
+    def _start_log(self):
+        
+        
+        try:
+            os.makedirs('{subject_dir}/logs'.format(subject_dir = self.subject_dir))
+            f = open('{subject_dir}/logs/log.txt'.format(subject_dir = self.subject_dir),'w')
+            f.close()
+            f.close()
+            self.log = []
+        except:
+            previous_logs = open('{subject_dir}/logs/log.txt'.format(subject_dir = self.subject_dir),'r').read().split('\n')[0:-1]
+            if self.overwrite is None:
+                self.log = previous_logs
+            else:
+                if self.overwrite=='all':
+                    self.log = []
+                    f = open('{subject_dir}/logs/log.txt'.format(subject_dir = self.subject_dir),'w')
+                    f.close()
+                else:
+                    for x in self.overwrite:
+                        previous_logs.remove(x)
+                    f = open('{subject_dir}/logs/log.txt'.format(subject_dir = self.subject_dir),'w')
+                    for p in previous_logs:
+                        f.write(p+'\n')
+                    f.close()
+                    self.log = previous_logs
+                    
+                        
+
+    def _add_to_log(self,name):
+        f = open('{subject_dir}/logs/log.txt'.format(subject_dir = self.subject_dir),'a')
+        f.write(name+'\n')
+        f.close()
+        self.log.append(name)
+        
+    
+        
+    def _add_dir(self,name):
+        
+        try:
+           os.makedirs('{subject_dir}/{name}'.format(subject_dir=self.subject_dir,name=name))
+        except:
+           shutil.rmtree('{subject_dir}/{name}'.format(subject_dir=self.subject_dir,name=name),ignore_errors=True)
+           os.makedirs('{subject_dir}/{name}'.format(subject_dir=self.subject_dir,name=name))
+        
+        self.directories[name] = '{subject_dir}/{name}'.format(subject_dir = self.subject_dir, name=name)
+            
         
     def _make_head_model(self):
         
-        if not os.path.isfile('{freesurfer_dir}/{subject}/bem/outer_skin_surface'.format(freesurfer_dir=self.freesurfer_dir, subject=self.subject)):
-            if not os.path.exists('{freesurfer_dir}/{subject}/bem'.format(freesurfer_dir=self.freesurfer_dir, subject=self.subject)):
-                os.makedirs('{freesurfer_dir}/{subject}/bem'.format(freesurfer_dir=self.freesurfer_dir, subject=self.subject))
-        
-            cmd ='cd {freesurfer_dir}/{subject}/bem; mri_watershed -surf surf {freesurfer_dir}/{subject}/mri/rawavg.mgz brain.mgz'.format(freesurfer_dir=self.freesurfer_dir, subject = self.subject)
-            os.system(cmd)
-        
-            for f in ['lh.surf_brain_surface','lh.surf_inner_skull_surface','lh.surf_outer_skin_surface','lh.surf_outer_skull_surface']:
-                cmd = 'mv {freesurfer_dir}/{subject}/bem/{f} {freesurfer_dir}/{subject}/bem/{f2}'.format(f=f,f2=f.split('lh.surf_')[1],freesurfer_dir=self.freesurfer_dir,subject=self.subject)
-                os.system(cmd)
+        if 'make_head_model' not in self.log:
+            make_head_model(self.subject,self.freesurfer_dir)
+            self._add_to_log('make_head_model')
         
 
-    def _to_mni(self,to_mni,mni_template):
-        pass #### TO DO    
-        
-        
+    def _to_mni(self,mni_template):
+        if 'to_mni' not in self.log:
+            self._add_dir('mni')
+            pass
+            ### TO DO
+            
+            self._add_to_log('to_mni')
+            
     def _read_brainsight_file(self):
-        
-        if self.brainsight_file is not None:
+        if 'read_brainsight_file' not in self.log:
+            self._add_dir('brainsight')
+            bs = BrainsightSessionFile(self.brainsight_file,out_dir='{subject_dir}/brainsight'.format(subject_dir=self.subject_dir))
+            self.brainsight_samples = BrainsightSamples('{subject_dir}/brainsight/Sample.txt'.format(subject_dir=self.subject_dir))
+            self.brainsight_targets = BrainsightTargets('{subject_dir}/brainsight/Target.txt'.format(subject_dir=self.subject_dir), self.subject, self.freesurfer_dir)
+            if 'Electrode' in bs.tables_names:
+                self.brainsight_electrodes = BrainsightElectrodes('{subject_dir}/brainsight/Electrode.txt'.format(subject_dir=self.subject_dir))
+            
+            self._add_to_log('read_brainsight_file')
+            
+        else:
             bs = BrainsightSessionFile(self.brainsight_file,out_dir='{subject_dir}/brainsight'.format(subject_dir=self.subject_dir))
             self.brainsight_samples = BrainsightSamples('{subject_dir}/brainsight/Sample.txt'.format(subject_dir=self.subject_dir))
             self.brainsight_targets = BrainsightTargets('{subject_dir}/brainsight/Target.txt'.format(subject_dir=self.subject_dir), self.subject, self.freesurfer_dir)
@@ -95,13 +140,11 @@ class StimProject(object):
                 self.brainsight_electrodes = BrainsightElectrodes('{subject_dir}/brainsight/Electrode.txt'.format(subject_dir=self.subject_dir))
             
 
-    def summary(self,plot_pulses=False, overwrite=False):
+    def summary(self,plot_pulses=False):
        
-       if self.brainsight_file:
-           if os.path.exists('{subject_dir}/brainsight/summary.html'.format(subject_dir=self.subject_dir))==False or overwrite==True:
-               self._brainsight_summary(plot_pulses)
-           else:
-               print 'brainsight summary exists.If you wish to overwrite, change overwrite to True!'
+       if 'summary' not in self.log:
+            self._brainsight_summary(plot_pulses) 
+            self._add_to_log('summary')
 
     def _brainsight_summary(self,plot_pulses):
    
@@ -135,7 +178,7 @@ class StimProject(object):
                    pulses_coords = pulses_coords[idx]
                    pulses = FreesurferCoords(pulses_coords,self.subject,self.freesurfer_dir)
                    prefix=session+'_'+t+'_'+str(j)
-                   plot_points_fast(pulses, map_surface='pial', annot='aparc', annot_alpha=1,show_average=True, out_dir = os.path.join(self.brainsight_dir, 'images'), prefix=prefix)
+                   plotting_points_fast(pulses, map_surface='pial', annot='aparc',show_average=True, out_dir = os.path.join(self.directories['brainsight'], 'images'), prefix=prefix)
                    html.add_paragraph('&nbsp&nbsp&nbsp -> Target: '+ t + ' (start:' + str(seq[0])+ ', end:' + str(seq[1]) + ', stimulations:' + str(seq[1]-seq[0]+1)+')<br><br>')
                    html.add_image('./images/'+prefix+'.png',200,900,'middle')
                    html.add_paragraph('<br><br>')
@@ -149,7 +192,9 @@ class StimProject(object):
 
         
             
-            
+
+        
+                    
 
     
     
