d2911649e2bba3b4df5e9ae145b2b00b5d33e6fe
Ehsan Tadayon <sunny.tadayon@gmail.com>
2020-03-23 18:04:18 -0400

    removes return_as_array

--- pynetstim/tms_simulation.py
+++ pynetstim/tms_simulation.py
@@ -38,7 +38,7 @@ class Simnibs(object):
             t12simnibs_reg = self.register_T1_to_simnibs()
             
             targets_ras = img2img_coord_register(targets_ras, img_file=self.orig_t1, dest_img=self.simnibs_t1, method='linear',
-                    linear_reg_file=t12simnibs_reg,return_as_array=True)
+                    linear_reg_file=t12simnibs_reg)
                 
 
         if project2skin is True:
